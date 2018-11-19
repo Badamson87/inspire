6eef21fcf7efd32b4ec66cb212ef33c5b2465a93
Brian Adamson <brianadamson87@gmail.com>
2018-11-18 17:27:56 -0700

    quote author

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -10,7 +10,10 @@ export default class QuoteController {
 
 	getQuote() {
 		qs.getQuote(function (quote) {
+			console.log(quote)
 			document.getElementById("quote").innerText = `${quote.quote.body}`
+			document.getElementById("author").innerText = ` - ${quote.quote.author}`
+
 		})
 	}
 }
